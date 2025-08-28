cc2f557f4499a9d0a5d2ce83d18e26cb9d4f2ef4
Rafael Gonçalves <rafael.gon123@gmail.com>
2025-08-28 21:58:59 +0100

    add eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,62 @@
-// @ts-check
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const prettierPlugin = require('eslint-plugin-prettier');
 
 module.exports = tseslint.config(
   {
     files: ['**/*.ts'],
     extends: [
-      // @ts-ignore
-      'plugin:prettier/recommended',
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
+      ...angular.configs.tsRecommended
     ],
     processor: angular.processInlineTemplates,
+    plugins: {
+      prettier: prettierPlugin
+    },
     rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
-      ],
+      // Prettier
+      'prettier/prettier': 'error',
+
+      // TypeScript
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/member-ordering': 'warn',
+
+      // Angular
+      '@angular-eslint/no-empty-lifecycle-method': 'warn',
+      '@angular-eslint/no-input-rename': 'error',
+      '@angular-eslint/no-output-on-prefix': 'error',
+      '@angular-eslint/use-lifecycle-interface': 'warn',
+      '@angular-eslint/use-pipe-transform-interface': 'error',
       '@angular-eslint/component-selector': [
         'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
+        { type: 'element', prefix: 'app', style: 'kebab-case' }
       ],
-      'prettier/prettier': 'error',
-    },
+      '@angular-eslint/directive-selector': [
+        'error',
+        { type: 'attribute', prefix: 'app', style: 'camelCase' }
+      ],
+
+      // ESLint
+      eqeqeq: ['error', 'always'],
+      curly: 'error',
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-duplicate-imports': 'error',
+      'arrow-body-style': ['error', 'as-needed'],
+      'no-shadow': 'warn'
+    }
   },
   {
     files: ['**/*.html'],
     extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
-    rules: {},
+    rules: {}
   }
 );
