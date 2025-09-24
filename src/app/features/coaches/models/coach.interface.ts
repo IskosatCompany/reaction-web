0d7865e646a6eb650edcf2f37044bcbb1df02087
Rafael Gonçalves <rafael.gon123@gmail.com>
2025-09-24 21:31:52 +0100

    chore: add base models

---
+++ src/app/features/coaches/models/coach.interface.ts
@@ -0,0 +1,13 @@
+export interface Coach {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  expertise: string;
+  color: string;
+  nif: string;
+  professionalCardNumber: string;
+  civilInsurance: string;
+  workInsurance: string;
+}
