0a01a6a715ed608622460c83c64c78988d19f03f
Rafael Gonçalves <45217371+RafaelFaria98@users.noreply.github.com>
2025-12-15 21:40:16 +0000

    feat(evaluation): back to client (#48)

--- src/app/features/clients/components/evaluations/evaluation-form/evaluation-form.component.ts
+++ src/app/features/clients/components/evaluations/evaluation-form/evaluation-form.component.ts
@@ -12,29 +12,30 @@ import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatStepperModule } from '@angular/material/stepper';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { IS_MOBILE } from '../../../../../core/tokens/mobile.token';
 import { CoachApiService } from '../../../../coaches/api/coach-api.service';
-import { ProfessionalPhysicalForm } from './professional-physical-form/professional-physical-form';
-import { ClinicalHistoryForm } from './clinical-history-form/clinical-history-form';
-import { MainComplaintsFormComponent } from './main-complaints-form/main-complaints-form';
-import { WellBeingFormComponent } from './well-being-form/well-being-form';
-import { DiagnosisTreatmentFormComponent } from './diagnosis-treatment-form/diagnosis-treatment-form';
-import { ActivatedRoute } from '@angular/router';
-import { GeneralInfoFormComponent } from './general-info-form/general-info-form';
-import { ClinicalEvaluationFormComponent } from './clinical-evaluation-form/clinical-evaluation-form';
 import { Authorization } from '../../../models/evaluation/authorization.model';
 import { ClinicalEvaluation } from '../../../models/evaluation/clinical-evaluation.model';
 import { ClinicalHistory } from '../../../models/evaluation/clinical-history.model';
 import { DiagnosisAndTreatment } from '../../../models/evaluation/diagnosis-treatment.model';
+import { Evaluation } from '../../../models/evaluation/evaluation.model';
 import { EvaluationGeneralInfo } from '../../../models/evaluation/general-info.model';
 import { MainComplaints } from '../../../models/evaluation/main-complaints.model';
 import { ProfessionalAndPhysicalData } from '../../../models/evaluation/professional-physical.model';
 import { WellBeing } from '../../../models/evaluation/well-being.model';
-import { Evaluation } from '../../../models/evaluation/evaluation.model';
-import { IS_MOBILE } from '../../../../../core/tokens/mobile.token';
+import { ClinicalEvaluationFormComponent } from './clinical-evaluation-form/clinical-evaluation-form';
+import { ClinicalHistoryForm } from './clinical-history-form/clinical-history-form';
+import { DiagnosisTreatmentFormComponent } from './diagnosis-treatment-form/diagnosis-treatment-form';
+import { GeneralInfoFormComponent } from './general-info-form/general-info-form';
+import { MainComplaintsFormComponent } from './main-complaints-form/main-complaints-form';
+import { ProfessionalPhysicalForm } from './professional-physical-form/professional-physical-form';
+import { WellBeingFormComponent } from './well-being-form/well-being-form';
 
 @Component({
   selector: 'app-evaluation-form',
@@ -54,7 +55,9 @@ import { IS_MOBILE } from '../../../../../core/tokens/mobile.token';
     WellBeingFormComponent,
     DiagnosisTreatmentFormComponent,
     GeneralInfoFormComponent,
-    ClinicalEvaluationFormComponent
+    ClinicalEvaluationFormComponent,
+    RouterLink,
+    MatIconModule
   ],
   templateUrl: './evaluation-form.component.html',
   styleUrl: './evaluation-form.component.scss',
