ece96b831e592f8bbef6fc67e9233063c0ecef02
Bruno Mendes <brunomendesit11@gmail.com>
2026-01-02 18:09:38 +0000

    fix: session title overflow

--- src/app/features/sessions/components/sessions-accordion/sessions-accordion.ts
+++ src/app/features/sessions/components/sessions-accordion/sessions-accordion.ts
@@ -3,26 +3,32 @@ import { ChangeDetectionStrategy, Component, computed, inject, input } from '@an
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
-import { Subject } from 'rxjs';
-import { AuthenticationService } from '../../../authentication/services/authentication.service';
 import { Session, SessionStatus, SessionStatusLabel } from '../../models/session.interface';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { IS_MOBILE } from '../../../../core/tokens/mobile.token';
+import { FormatCoachPipe } from '../../../../ui/pipes/format-coach.pipe';
 
 @Component({
   selector: 'app-sessions-accordion',
-  imports: [MatExpansionModule, DatePipe, MatButtonModule, MatIconModule],
+  imports: [MatExpansionModule, DatePipe, MatButtonModule, MatIconModule, FormatCoachPipe],
   templateUrl: './sessions-accordion.html',
   styleUrl: './sessions-accordion.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SessionsAccordion {
-  authService = inject(AuthenticationService);
+  readonly snackBar = inject(MatSnackBar);
+  readonly isMobile = inject(IS_MOBILE);
 
   session = input.required<Session>();
 
   isSessionPending = computed(() => this.session().status === SessionStatus.Pending);
 
   sessionStatusEnum = SessionStatus;
-  evaluationEditSubject$ = new Subject<void>();
-  canEdit = this.authService.isAdmin();
   statusLabels = SessionStatusLabel;
+
+  handlePendingSession(): void {
+    if (this.isSessionPending()) {
+      this.snackBar.open('Esta sessão está pendente e não pode ser editada.');
+    }
+  }
 }
